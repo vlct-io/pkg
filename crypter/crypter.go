afb639443a8585708d2f20949bb0faef1e07efd4
Leonardo <leo@vlct.io>
2018-11-05 13:05:20 +0100

    change interface contract

--- crypter/crypter.go
+++ crypter/crypter.go
@@ -2,6 +2,6 @@ package crypter
 
 // Crypter defines the methods for encryption and decryption of data.
 type Crypter interface {
-	Encrypt(plainData string) (cipherData string, err error)
-	Decrypt(cipherData string) (plainData string, err error)
+	Encrypt(plainText []byte) (cipherText []byte, err error)
+	Decrypt(cipherText []byte) (plainText []byte, err error)
 }
