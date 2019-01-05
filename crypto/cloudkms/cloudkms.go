1002f1554eec26645ed8d336261883aab4647679
Leonardo <leo@vlct.io>
2019-01-05 16:28:13 +0100

    cloudkms: expand API
    Signed-off-by: Leonardo <leo@vlct.io>

--- crypto/cloudkms/cloudkms.go
+++ crypto/cloudkms/cloudkms.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/pkg/errors"
 
 	"github.com/vlct-io/pkg/crypto"
 	"golang.org/x/oauth2/google"
@@ -23,10 +28,11 @@ import (
 // Cloud KMS pricing: https://cloud.google.com/kms/pricing
 //
 type cloudKMS struct {
-	ProjectID   string
-	LocationID  string
-	KeyRingID   string
-	CryptoKeyID string
+	ProjectID    string
+	LocationID   string
+	KeyRingID    string
+	CryptoKeyID  string
+	authedClient *http.Client
 }
 
 // validate interface conformity.
@@ -34,21 +40,61 @@ var _ crypto.Crypter = cloudKMS{}
 
 // New makes a crypto.Crypter.
 func New(projectID, locationID, keyRingID, cryptoKeyID string) crypto.Crypter {
-	return cloudKMS{
-		ProjectID:   projectID,
-		LocationID:  locationID,
-		KeyRingID:   keyRingID,
-		CryptoKeyID: cryptoKeyID,
+	ctx := context.Background()
+	authedClient, err := google.DefaultClient(ctx, cloudkms.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
 	}
+	kms := cloudKMS{
+		ProjectID:    projectID,
+		LocationID:   locationID,
+		KeyRingID:    keyRingID,
+		CryptoKeyID:  cryptoKeyID,
+		authedClient: authedClient,
+	}
+	log.Println("ensuring keys")
+	err = kms.EnsureKeys()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return kms
+}
+
+func (kms cloudKMS) EnsureKeys() error {
+	var err error
+	kmsService, err := cloudkms.New(kms.authedClient)
+	if err != nil {
+		return err
+	}
+	// The resource name of the key rings.
+	resourceName := fmt.Sprintf("projects/%s/locations/%s", kms.ProjectID, kms.LocationID)
+	// Do RPC call
+	res, err := kmsService.Projects.Locations.KeyRings.List(resourceName).Do()
+	if err != nil {
+		return errors.Wrap(err, "failed to list key rings")
+	}
+	// find our keyring, exit when we do
+	for _, keyRing := range res.KeyRings {
+		if strings.Contains(keyRing.Name, kms.KeyRingID) {
+			log.Printf("KeyRing found! %v\n", keyRing.Name)
+			return nil
+		}
+	}
+	// we don't have a keyring, let's make one
+	log.Println("KeyRing empty, generating...")
+	if err := kms.createKeyring(kms.KeyRingID); err != nil {
+		log.Println("failed to create keyring:", err)
+	}
+	if err := kms.createCryptoKey(kms.KeyRingID, kms.CryptoKeyID); err != nil {
+		return errors.Wrap(err, "failed to create key")
+	}
+
+	return errors.New("keyRing not found! Generate one at: 'https://console.cloud.google.com/security/kms?authuser=1&project=vaultex-net'")
 }
 
 // Encrypt handles all cloudKMS service operations to successfully encrypt the plainText.
 func (kms cloudKMS) Encrypt(plaintext []byte) ([]byte, error) {
-	ctx := context.Background()
-	client, err := google.DefaultClient(ctx, cloudkms.CloudPlatformScope)
-	if err != nil {
-		return nil, err
-	}
+	client := kms.authedClient
 	cloudKMSService, err := cloudkms.New(client)
 	if err != nil {
 		return nil, err
@@ -68,11 +114,7 @@ func (kms cloudKMS) Encrypt(plaintext []byte) ([]byte, error) {
 
 // Decrypt handles all cloudKMS service operations to successfully Decrypt the cypherText.
 func (kms cloudKMS) Decrypt(ciphertext []byte) ([]byte, error) {
-	ctx := context.Background()
-	client, err := google.DefaultClient(ctx, cloudkms.CloudPlatformScope)
-	if err != nil {
-		return nil, err
-	}
+	client := kms.authedClient
 	cloudKMSService, err := cloudkms.New(client)
 	if err != nil {
 		return nil, err
@@ -88,4 +130,41 @@ func (kms cloudKMS) Decrypt(ciphertext []byte) ([]byte, error) {
 		return nil, err
 	}
 	return base64.StdEncoding.DecodeString(resp.Plaintext)
+}
+
+func (kms cloudKMS) createKeyring(keyRing string) error {
+	client, err := cloudkms.New(kms.authedClient)
+	if err != nil {
+		return err
+	}
+	location := kms.LocationID
+	parent := fmt.Sprintf("projects/%s/locations/%s", kms.ProjectID, location)
+
+	_, err = client.Projects.Locations.KeyRings.Create(
+		parent, &cloudkms.KeyRing{}).KeyRingId(keyRing).Do()
+	if err != nil {
+		return err
+	}
+	log.Print("Created key ring.")
+	return nil
+}
+
+func (kms cloudKMS) createCryptoKey(keyRing, key string) error {
+	client, err := cloudkms.New(kms.authedClient)
+	if err != nil {
+		return err
+	}
+	location := kms.LocationID
+	parent := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s", kms.ProjectID, location, keyRing)
+	purpose := "ENCRYPT_DECRYPT"
+	_, err = client.Projects.Locations.KeyRings.CryptoKeys.Create(
+		parent, &cloudkms.CryptoKey{
+			Purpose: purpose,
+		}).CryptoKeyId(key).Do()
+	if err != nil {
+		return err
+	}
+	log.Println("Created crypto key.")
+
+	return nil
 }
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/vlct-io/pkg
+
+require (
+	github.com/pkg/errors v0.8.0
+	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890
+	google.golang.org/api v0.1.0
+)
