f87a24b0d47450b3d07bf79b198cca456fcea354
Leonardo <leo@vlct.io>
2019-01-18 16:13:28 +0100

    add cloudkms signing methods
    Signed-off-by: Leonardo <leo@vlct.io>

--- crypto/cloudkms/cloudkms.go
+++ crypto/cloudkms/cloudkms.go
@@ -2,8 +2,14 @@ package cloudKMS
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/asn1"
 	"encoding/base64"
+	"encoding/pem"
 	"fmt"
+	"math/big"
 	"net/http"
 	"strings"
 
@@ -31,6 +37,7 @@ type cloudKMS struct {
 	LocationID   string
 	KeyRingID    string
 	CryptoKeyID  string
+	SigningKeyID string
 	authedClient *http.Client
 }
 
@@ -49,7 +56,8 @@ func New(projectID, locationID, keyRingID, cryptoKeyID string) crypto.Crypter {
 		ProjectID:    projectID,
 		LocationID:   locationID,
 		KeyRingID:    keyRingID,
-		CryptoKeyID:  cryptoKeyID,
+		CryptoKeyID:  cryptoKeyID + "_crypto",
+		SigningKeyID: cryptoKeyID + "_sign",
 		authedClient: authedClient,
 	}
 	err = kms.EnsureKeys()
@@ -80,14 +88,14 @@ func (kms cloudKMS) EnsureKeys() error {
 		}
 	}
 	// we don't have a keyring, let's make one
-	log.Println("KeyRing empty, generating...")
+	log.Println("generating KeyRing...")
 	if err := kms.createKeyring(kms.KeyRingID); err != nil {
 		log.Println("failed to create keyring:", err)
 	}
-	if err := kms.createCryptoKey(kms.KeyRingID, kms.CryptoKeyID); err != nil {
+	// TODO(leo): add cryptoKey search and print like keyRing
+	if err := kms.createCryptoKeys(); err != nil {
 		return errors.Wrap(err, "failed to create key")
 	}
-
 	return errors.New("keyRing not found! Generate one at: https://console.cloud.google.com/security/kms")
 }
 
@@ -131,6 +139,73 @@ func (kms cloudKMS) Decrypt(ciphertext []byte) ([]byte, error) {
 	return base64.StdEncoding.DecodeString(resp.Plaintext)
 }
 
+// Sign will sign a plaintext message using a saved asymmetric private key.
+// example keyName: "projects/PROJECT_ID/locations/global/keyRings/RING_ID/cryptoKeys/KEY_ID/cryptoKeyVersions/1"
+func (kms cloudKMS) Sign(message []byte) ([]byte, error) {
+	var err error
+	kmsService, err := cloudkms.New(kms.authedClient)
+	if err != nil {
+		return nil, err
+	}
+	// Find the digest of the message.
+	digest := sha256.New()
+	digest.Write(message)
+	parentName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/%s",
+		kms.ProjectID, kms.LocationID, kms.KeyRingID, kms.CryptoKeyID)
+	// Build the signing request.
+	req := &cloudkms.AsymmetricSignRequest{
+		Digest: &cloudkms.Digest{
+			Sha256: string(digest.Sum(nil)),
+		},
+	}
+	// Call the API.
+	res, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.CryptoKeyVersions.AsymmetricSign(parentName, req).Do()
+	if err != nil {
+		return nil, fmt.Errorf("asymmetric sign request failed: %+v", err)
+	}
+	return base64.StdEncoding.DecodeString(res.Signature)
+}
+
+// verifySignatureEC will verify that an 'EC_SIGN_P256_SHA256' signature is valid for a given message.
+// example keyName: "projects/PROJECT_ID/locations/global/keyRings/RING_ID/cryptoKeys/KEY_ID/cryptoKeyVersions/1"
+func (kms cloudKMS) Verify(signature, message []byte) error {
+	var err error
+	kmsService, err := cloudkms.New(kms.authedClient)
+	if err != nil {
+		return err
+	}
+	parentName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/%s",
+		kms.ProjectID, kms.LocationID, kms.KeyRingID, kms.CryptoKeyID)
+	// Retrieve the public key from KMS.
+	res, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.CryptoKeyVersions.GetPublicKey(parentName).Do()
+	if err != nil {
+		return fmt.Errorf("failed to fetch public key: %+v", err)
+	}
+	// Parse the key.
+	block, _ := pem.Decode([]byte(res.Pem))
+	abstractKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return fmt.Errorf("failed to parse public key: %+v", err)
+	}
+	ecKey, ok := abstractKey.(*ecdsa.PublicKey)
+	if !ok {
+		return fmt.Errorf("key '%s' is not EC", abstractKey)
+	}
+	// Verify Elliptic Curve signature.
+	var parsedSig struct{ R, S *big.Int }
+	_, err = asn1.Unmarshal(signature, &parsedSig)
+	if err != nil {
+		return fmt.Errorf("failed to parse signature bytes: %+v", err)
+	}
+	hash := sha256.New()
+	hash.Write(message)
+	digest := hash.Sum(nil)
+	if !ecdsa.Verify(ecKey, digest, parsedSig.R, parsedSig.S) {
+		return errors.New("signature verification failed")
+	}
+	return nil
+}
+
 func (kms cloudKMS) createKeyring(keyRing string) error {
 	client, err := cloudkms.New(kms.authedClient)
 	if err != nil {
@@ -148,20 +223,33 @@ func (kms cloudKMS) createKeyring(keyRing string) error {
 	return nil
 }
 
-func (kms cloudKMS) createCryptoKey(keyRing, key string) error {
+func (kms cloudKMS) createCryptoKeys() error {
 	client, err := cloudkms.New(kms.authedClient)
 	if err != nil {
 		return err
 	}
 	location := kms.LocationID
-	parent := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s", kms.ProjectID, location, keyRing)
-	purpose := "ENCRYPT_DECRYPT"
+	// setup Key ring
+	parent := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s", kms.ProjectID, location, kms.KeyRingID)
+	// create ENCRYPT_DECRYPT
 	_, err = client.Projects.Locations.KeyRings.CryptoKeys.Create(
 		parent, &cloudkms.CryptoKey{
-			Purpose: purpose,
-		}).CryptoKeyId(key).Do()
+			Purpose: "ENCRYPT_DECRYPT",
+		}).CryptoKeyId(kms.CryptoKeyID).Do()
 	if err != nil {
-		return err
+		log.Println(err)
+	}
+	log.Printf("Created crypto key %v\n", parent)
+	// create ASYMMETRIC_SIGN
+	_, err = client.Projects.Locations.KeyRings.CryptoKeys.Create(
+		parent, &cloudkms.CryptoKey{
+			Purpose: "ASYMMETRIC_SIGN",
+			VersionTemplate: &cloudkms.CryptoKeyVersionTemplate{
+				Algorithm: "EC_SIGN_P256_SHA256",
+			},
+		}).CryptoKeyId(kms.SigningKeyID).Do()
+	if err != nil {
+		log.Println(err)
 	}
 	log.Printf("Created crypto key %v\n", parent)
 
